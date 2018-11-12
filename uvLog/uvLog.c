783a7255e877e36217b134efbeea6d557cf9a9dc
wlla <wlla@jiangsuits.com>
2018-11-12 18:21:00 +0800

    添加uvLog模块

---
+++ uvLog/uvLog.c
@@ -0,0 +1,10 @@
+#include "st.h"
+#include "uvLog.h"
+
+int uv_log_init(uv_log_handle_t *h) {
+    return 0;
+}
+
+int uv_log_init_conf(uv_log_handle_t *h, char* conf_path) {
+    return 0;
+}
---
+++ uvLog/uvLog.h
@@ -0,0 +1,11 @@
+#ifndef _UV_LOG_H_
+#define _UV_LOG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
