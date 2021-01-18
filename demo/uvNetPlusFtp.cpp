15932623b47df73417366e7a206f281f9225319a
wlla <wlla@jiangsuits.com>
2021-01-18 09:41:59 +0800

    开始写ftp服务端

--- demo/uvNetPlusFtp.cpp
+++ demo/uvNetPlusFtp.cpp
@@ -1,5 +1,5 @@
 #include "util.h"
-#include "utilc_api.h"
+#include "utilc.h"
 #include "uv.h"
 #include "uvnetplus.h"
 #include <stdio.h>
@@ -13,59 +13,59 @@ using namespace uvNetPlus::Ftp;
 CNet* net = NULL;
 
 //获取文件列表
-static void OnFtpList(CFtpRequest *req, list<CFtpFile> files) {
+static void OnFtpList(CFtpConnect *req, list<CFtpFile> files) {
     for(auto f:files) {
         Log::warning("%s %s %s", f.permission.c_str(), f.date.c_str(), f.name.c_str());
     }
 }
 
 //切换目录
-static void OnChangeWorkingDirectory(CFtpRequest *req) {
+static void OnChangeWorkingDirectory(CFtpConnect *req) {
     Log::debug("change working directory success");
     req->List(OnFtpList);
 }
 
 //获取文件列表
-static void OnNameList(CFtpRequest *req, std::list<std::string> names) {
+static void OnNameList(CFtpConnect *req, std::list<std::string> names) {
     for(auto str:names) {
         Log::warning(str.c_str());
     }
 }
 
 //上传文件
-static void OnUpload(CFtpRequest *req) {
+static void OnUpload(CFtpConnect *req) {
     Log::debug("upload success");
 }
 
 //下载文件
-static void OnDownload(CFtpRequest *req, char* data, uint32_t size) {
+static void OnDownload(CFtpConnect *req, char* data, uint32_t size) {
     FILE *f = fopen("d:\\test2.jpg", "wb+");
     fwrite(data, 1, size, f);
     fclose(f);
 }
 
 //创建文件夹
-static void OnMakeDirectory(CFtpRequest *req) {
+static void OnMakeDirectory(CFtpConnect *req) {
     Log::debug("make directory success");
 }
 
 //删除文件夹
-static void OnRemoveDirectory(CFtpRequest *req) {
+static void OnRemoveDirectory(CFtpConnect *req) {
     Log::debug("remove directory success");
 }
 
 //删除文件
-static void OnDeleteFile(CFtpRequest *req) {
+static void OnDeleteFile(CFtpConnect *req) {
     Log::debug("delete file sucess");
 }
 
 //异常分支回调
-static void OnFtpCb(CFtpRequest *req, CFtpMsg *msg) {
+static void OnFtpCb(CFtpConnect *req, CFtpMsg *msg) {
     Log::error("%s", msg->replyStr.c_str());
 }
 
 // 登陆成功
-static void OnFtpLogin(CFtpRequest *req) {
+static void OnFtpLogin(CFtpConnect *req) {
     req->ChangeWorkingDirectory("/var/ftp/vioupload", OnChangeWorkingDirectory);
     /*
     req->NameList(OnNameList);
@@ -96,10 +96,10 @@ static void OnFtpLogin(CFtpRequest *req) {
 
 int main()
 {
-    Log::open(Log::Print::both, Log::Level::debug, "./log/unNetPlusFtp.txt");
+    Log::open(Log::Print::both, Log::Level::debug, "./log/unNetPlusFtp/log.txt");
     net = CNet::Create();
     CFtpClient* ftpClient = CFtpClient::Create(net);
-    ftpClient->Request("192.168.2.111", 21, "ftp", "123", OnFtpLogin, OnFtpCb);
+    ftpClient->Connect("192.168.2.111", 21, "ftp", "123", OnFtpLogin, OnFtpCb);
 
     sleep(INFINITE);
 }
